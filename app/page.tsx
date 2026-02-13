a9eb285afbf5f65b186a04476cbd5ef9dcfa392d
Cweeesh <carlosjosephdizon19@gmail.com>
2026-02-13 17:17:09 +0800

    Add picture

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
-     <main className="flex min-h-screen w-full max-w-1xl flex-col items-center justify-center py-32 px-16 bg-white">
-    
-    <div className="flex flex-col items-center gap-10">
-      <h1 className="max-w-xs text-center text-3xl font-semibold leading-10 tracking-tight text-black">
-        Hello world po
-      </h1>
-    </div>
+    <div className="relative flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+      <main className="flex min-h-screen w-full max-w-none flex-col items-center justify-center py-32 px-16 bg-white">
+        
+        <div className="flex flex-col items-center gap-10">
+          <h1 className="w-full text-center text-[100px] font-semibold leading-none tracking-tighter text-black whitespace-nowrap">
+            Hi! I am CJ
+          </h1>
+        </div>
 
-  </main>
-</div>
+      </main>
+
+      <div className="fixed bottom-1 right-8">
+        <Image 
+          src="/cj.png" 
+          alt="CJ" 
+          width={700} 
+          height={700} 
+          className="object-contain"
+          priority
+        />
+      </div>
+    </div>
   );
 }
